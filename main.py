82e8588a3f2def4f7b541285e3ef74793ec98975
Selim Cavas <selim.cavas@ozu.edu.tr>
2023-07-13 08:43:56 +0300

    added usb mode switching and modem config

--- main.py
+++ main.py
@@ -8,32 +8,32 @@
 
 import serial
 import time
+import os
 
 # Function to wait for the response of the modem
 
 
 def waitResponse():
     response = b''
-    while not response.endswith(b'\r\nOK\r\n') and not response.endswith(b'\r\nERROR\r\n'):
+    while not response.endswith(b'\r\nOK\r\n') and not response.endswith(b'\r\nERROR\r\n') and 'ERROR' not in response.decode():
 
         response += modem.readline()
         response_str = response.decode()
         response_str = response_str[2:-2]
         print(response_str)
 
-    if response.endswith(b'\r\nOK\r\n'):
-        print('Response received: OK')
-    elif response.endswith(b'\r\nERROR\r\n'):
-        print('Response received: ERROR')
+    time.sleep(2)
 
-    time.sleep(1)
-
-
+# Switch the modem to the modem mode
+os.system('sudo usb_modeswitch -c /etc/k4203-modem.conf')
+# Stop the ModemManager service
+os.system('sudo systemctl stop ModemManager.service')
 # Establish connection with the modem
 modem = serial.Serial(port='/dev/ttyUSB0', baudrate=9600,
                       rtscts=True, dsrdtr=True, timeout=0.1)
 
 print('Modem is ready!')
+
 try:
     time.sleep(0.5)
     print('Sending AT CURC command')
